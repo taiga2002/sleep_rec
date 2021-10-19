be2ca8434d5404d4e5e8c16280ad9fe0312a5db0
Taiga Kitao <taiga20022002@gmail.com>
2021-10-18 17:59:08 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by 北尾　大河 on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
